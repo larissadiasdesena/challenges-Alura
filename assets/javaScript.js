1103a0708019aef40d6dfea97a1ac7d2f80e777f
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-16 18:55:33 -0300

    update

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,11 +1,11 @@
 
-let encript = document.getElementById("inputCript")
-let descrip = document.getElementById("inputDesc")
-let copiar = document.getElementById("inputCopiar")
+let encript = document.getElementById("textCript")
+let descrip = document.getElementById("textDesc")
+let copiar = document.getElementById("textCopiar")
 
 
 // Botão encript
-document.getElementById("buttonCript").addEventListener("click", function(){
+document.getElementById("btnCript").addEventListener("click", function(){
     let input = encript.value  
     let modificado = input.normalize("NFD") // normaliza caracteres especiais
     modificado = modificado.toLocaleLowerCase(); // normaliza letras maiúsculas
@@ -16,18 +16,14 @@ document.getElementById("buttonCript").addEventListener("click", function(){
         const elemento = document.createElement('div')
         elemento.id = "mensagemErro"
         elemento.innerHTML = "<p>Apenas letras minúsculas e sem acento.</p>"
-        document.body.appendChild(elemento)
-        setTimeout(removeErro, 2000)
-        
+        document.getElementById("div-cripDes").appendChild(elemento)
+        setTimeout(removeErro, 2000)        
         
         function removeErro(){
             document.getElementById('mensagemErro').remove()
-        }
-        
-       
-    } else {     
-        
-        
+        }     
+
+    } else {         
         let result = input.replaceAll("e", "enter")
         result = result.replaceAll("i", "imes")
         result = result.replaceAll("a", "ai")
@@ -35,18 +31,11 @@ document.getElementById("buttonCript").addEventListener("click", function(){
         result = result.replaceAll("u", "ufat")
         encript.value = ""
         descrip.value = result
-
-    }
-
-    
+    }  
 })
 
-
 // botão descripte
-document.getElementById("buttonDescr").addEventListener("click", function(){
-    
-
-   
+document.getElementById("btnDesc").addEventListener("click", function(){  
     let input = descrip.value
 
     let result = input.replaceAll("enter", "e")
@@ -56,25 +45,19 @@ document.getElementById("buttonDescr").addEventListener("click", function(){
     result = result.replaceAll("ufat", "u")
     descrip.value = ""
     encript.value = result
-  
-
-
 })
 
-
 // Botão copiar mensagem encriptada
-document.getElementById("buttonCopiar").addEventListener("click", function(){
-
-    let qualquer = document.getElementById("inputCopiar")
-    navigator.clipboard.writeText(qualquer.value);
+document.getElementById("btnCopiar").addEventListener("click", function(){
+    let qualquer = document.getElementById("textCopiar")
+    navigator.clipboard.writeText(qualquer.value)
     copiar.value = ""
 
     const elemento = document.createElement('div')
     elemento.id = "Copiar"
     elemento.innerHTML = "<p>Texto Copiado</p>"
-    document.getElementById("div-cop").appendChild(elemento)
+    document.getElementById("div-copiar").appendChild(elemento)
     setTimeout(removeCopiar, 2000)
-
 })
 
 // Remover mensagem flutuante
@@ -82,13 +65,8 @@ function removeCopiar(){
     document.getElementById('Copiar').remove()
 }
 
-
 // Seta de encriptar para copiar
 document.getElementById("seta").addEventListener("click", function(){
-    document.getElementById("inputCopiar").value = descrip.value
+    document.getElementById("textCopiar").value = descrip.value
     descrip.value = ""
-
 })
-
-
-
